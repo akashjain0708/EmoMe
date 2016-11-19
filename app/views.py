f1f5e804b58916112d6c2c7341f6b2eb14b56e47
Sheivin Goyal <sheivingoyal@gmail.com>
2016-11-19 06:15:46 -0600

    Charts

--- app/views.py
+++ app/views.py
@@ -5,3 +5,9 @@ from flask import render_template, request
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/temp')
+def chart():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    return render_template('chart.html', values=values, labels=labels)
