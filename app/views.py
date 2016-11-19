eea14117310de9a9007bc46bef5be057ae9643e7
Akash Jain <akashjain0708@gmail.com>
2016-11-19 13:43:13 -0600

    Charts

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, request
 import urllib
+from clarifai_cont.videoController import VideoController
 
 @app.route('/')
 @app.route('/index')
@@ -13,15 +14,13 @@ def chart():
     values = [10,9,8,7,6,4,7,8]
     return render_template('chart.html', values=values, labels=labels)
 
-@app.route('/submitData', methods=['POST'])
-def post_chart_data():
-    if request.method == 'POST':
-        link_to_video = request.json
+@app.route('/getCharts', methods=['GET'])
+def get_charts():
+    data_set = []
+    if request.method == 'GET':
+        link_to_video = request.json['videoURL']
         testfile = urllib.URLopener()
         testfile.retrieve(link_to_video, "./clarifai_cont/frame_images/user_video.webm")
-
-@app.route('/getChart', methods=['GET'])
-def display_chart():
-    video_obj = VideoController()
-    data_set = video_obj.getDataFromVideo('user_video.webm')
+        video_obj = VideoController()
+        data_set = video_obj.getDataFromVideo('user_video.webm')
     return render_template('chart.html', dataset = data_set)
