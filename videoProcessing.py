44422cde356d5f295420155e76944ee6a1619a8f
Sheivin Goyal <sheivingoyal@gmail.com>
2016-11-19 01:12:33 -0600

    Now splits frames from each second and returns the names of the images

--- videoProcessing.py
+++ videoProcessing.py
@@ -5,4 +5,14 @@ class VideoProcessing(object):
 		pass
 
 	def processVid(self, readVideo):
-		pass
+		fps = readVideo.get_meta_data()['fps']
+
+		stripFileNames = []
+		count = 1
+		for i, im in enumerate(readVideo):
+			if i % fps == 0:
+				imageio.imwrite("assets/splitImages/temp" + str(count) + ".jpg", im)
+				stripFileNames.append("temp" + str(count) + ".jpg")
+				count += 1
+
+		return stripFileNames
