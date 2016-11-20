1e17a116382bbbd702b76363c7143fde665871de
Abhishek Verma <abhiver222@gmail.com>
2016-11-20 06:14:26 -0600

    chart polar

--- app/static/js/chart.js
+++ app/static/js/chart.js
@@ -73,10 +73,10 @@ var createChart = function (xLabels, yPoints, yPoints_2, yPoints_3, chartID, bor
     });
 };
 
-var createPieChart = function(happyVal, sadVal, angerVal, neutralVal, chartID){
+var createPolarChart = function(happyVal, sadVal, angerVal, neutralVal, chartID){
     var ctx = document.getElementById(chartID);
-    var PieChart = new Chart(ctx,{
-        type: 'pie',
+    var polarChart = new Chart(ctx,{
+        type: 'polarArea',
         data: {
             labels: ['Happy','Sad','Angry','Neutral'],
             datasets: [{
@@ -171,4 +171,4 @@ createChart(x1, y1, y2, y3, "smileChart","rgba(75,192,192,1)" ,"#fff", "rgba(75,
 //var jsonData = {0: {'Smile': 0.50381887}, 1: {'Smile': 0.5244584}, 2: {'Smile': 0.37471813}, 3: {'Smile': 0.3662309}, 4: {'Smile': 0.38467777}, 5: {'Smile': 0.35506552}, 6: {'Smile': 0.35424682}, 7: {'Smile': 0.3941398}, 8: {'Smile': 0.41692236}, 9: {'Smile': 0.38992262}, 10: {'Smile': 0.33604342}, 11: {'Smile': 0.44526315}, 12: {'Smile': 0.38967356}, 13: {'Smile': 0.46438512}, 14: {'Smile': 0.33246452}, 15: {'Smile': 0.34840274}, 16: {'Smile': 0.34378657}, 17: {'Smile': 0.33653626}, 18: {'Smile': 0.48442918}, 19: {'Smile': 0.53641176}, 20: {'Smile': 0.531845}, 21: {'Smile': 0.44345507}, 22: {'Smile': 0.33793026}, 23: {'Smile': 0.33024272}, 24: {'Smile': 0.35893732}, 25: {'Smile': 0.35307083}, 26: {'Smile': 0.35426182}, 27: {'Smile': 0.36865106}, 28: {'Smile': 0.3226741}}
 //convertDataToChart('Smile', jsonData);
 
-createPieChart(40,20,15,25,'pieChart');
+createPolarChart(40,20,15,25,'polarChart');
