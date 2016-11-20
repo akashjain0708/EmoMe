052b7ebc4e59ad711fc5637519615f319e61ef70
Abhishek Verma <abhiver222@gmail.com>
2016-11-19 20:01:10 -0600

    template

--- clarifai_api.py
+++
@@ -1,9 +0,0 @@
-from clarifai.rest import ClarifaiApp, Image as ClImage
-
-app = ClarifaiApp()
-
-model = app.models.get('Emotion')
-
-# image = ClImage(filename='/assets/tester.jpg')
-
-print model.predict_by_url('https://samples.clarifai.com/metro-north.jpg')
